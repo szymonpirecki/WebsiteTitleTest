aae591ae807920bf645b063958cb54f4287c8cf5
szymonpirecki <szymon.pirecki@gmail.com>
2023-11-03 12:56:31 +0100

    [SUP_v5_Logger_5.2]

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -3,18 +3,25 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class TestBase {
+    private Logger log = LoggerFactory.getLogger("TestBase.class");
+
     public WebDriver driver;
 
     @BeforeEach
     void setDriver() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
+        log.info("Driver initialized properly");
     }
 
     @AfterEach
     void tearDown() {
         driver.quit();
+        log.info("Driver quit properly");
     }
+
 }
--- src/test/java/WebsiteTitleTest.java
+++ src/test/java/WebsiteTitleTest.java
@@ -1,17 +1,33 @@
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class WebsiteTitleTest extends TestBase {
 
+    private Logger log = LoggerFactory.getLogger("WebsiteTitleTest.class");
+
     @ParameterizedTest(name = "Test {index}: {2}")
     @CsvFileSource(resources = "shouldCheckWebsiteTitleParameters.csv")
     @Tag("REGRESSION")
     public void shouldCheckWebsiteTitle(String url, String expectedTitle, String testName) {
+        log.info("Start test: " + testName);
         driver.get(url);
+        log.debug("Going to the website: " + url);
         String title = driver.getTitle();
-        assertThat(title).isEqualTo(expectedTitle);
+        log.debug("Website title equals: " + title);
+        log.debug("Expected title equals: " + expectedTitle);
+        log.info("Comparing titles");
+        try {
+            assertThat(title).isEqualTo(expectedTitle);
+            log.info(testName + " passed");
+        } catch (AssertionError error) {
+            log.error(testName + " failed - The received title is different from the expected one");
+            throw error;
+        }
+        log.info(testName + " finalized");
     }
 }
